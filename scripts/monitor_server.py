b5538efc3af2a2e6f6ea8208418baa632672c504
Nils Bore <nilsbore@gmail.com>
2018-04-06 22:19:29 +0200

    Added a first version of the monitor server

---
+++ scripts/monitor_server.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+
+import rospy
+import sys
+#from __future__ import print_function
+import roslaunch_monitor.msg
+
+# Brings in the SimpleActionClient
+import actionlib
+
+# Brings in the messages used by the fibonacci action, including the
+# goal message and the result message.
+import actionlib_tutorials.msg
+
+class MonitorServer(object):
+
+    def feedback_cb(self, msg):
+        print msg
+
+    def launch(self, pkg, launch_file):
+        # Creates the SimpleActionClient, passing the type of the action
+        # (FibonacciAction) to the constructor.
+        self.client = actionlib.SimpleActionClient('/launch_monitor_server', roslaunch_monitor.msg.LaunchAction)
+
+        # Waits until the action server has started up and started
+        # listening for goals.
+        self.client.wait_for_server()
+
+        # Creates a goal to send to the action server.
+        goal = roslaunch_monitor.msg.LaunchGoal(pkg=pkg, launch_file=launch_file)
+
+        # Sends the goal to the action server.
+        self.client.send_goal(goal, feedback_cb=self.feedback_cb)
+
+        # Waits for the server to finish performing the action.
+        #client.wait_for_result()
+
+        # Prints out the result of executing the action
+        #return client.get_result()  # A FibonacciResult
+
+    def spin(self):
+
+        if not self.client.wait_for_server(timeout=rospy.Duration(0.5)):
+            rospy.loginfo("Server not reachable, quitting...")
+            return False
+
+        if rospy.is_shutdown():
+            rospy.loginfo("Shutting down launch server...")
+            self.client.cancel_goal()
+            self.client.wait_for_result()
+            print self.client.get_result()
+            rospy.loginfo("Finished shutting down launch server...")
+            return False
+        elif self.client.get_state() != actionlib.SimpleGoalState.ACTIVE and \
+             self.client.get_state() != actionlib.SimpleGoalState.PENDING:
+            rospy.loginfo("Launch server was aborted, quitting...")
+            return False
+        return True
+
+if __name__ == '__main__':
+    rospy.init_node('monitor_server')
+    monitor_server = MonitorServer()
+
+    monitor_server.launch(sys.argv[1], sys.argv[2])
+    while monitor_server.spin():
+        rospy.sleep(0.2)
+
